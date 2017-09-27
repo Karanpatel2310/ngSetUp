3670a834067788623c4f0f72e344d55a4c719704
karanpatel <karan.patel.sa@gmail.com>
2017-09-27 15:17:48 +0530

    Routing Demo completed

--- New/src/app/app.module.ts
+++ New/src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { ProductComponent } from './product/product.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+// import { SortPipe } from './app.sort';
+// import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // SortPipe,
+    AppComponent,
+    AccountComponent,
+    ProductComponent,
+    AboutusComponent,
+    ContactusComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'aboutus', component: AboutusComponent },
+      { path: 'contactus', component: ContactusComponent },
+      { path: 'account', component: AccountComponent },
+      { path: 'product', component: ProductComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full'},
+      { path: '**', component: HomeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
